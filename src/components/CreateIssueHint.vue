939cda11ea3071873de9505f5efe728902c557bc
FD2008 <fudesign2008@163.com>
2023-07-07 01:05:37 +0800

    feat: init project

---
+++ src/components/CreateIssueHint.vue
@@ -0,0 +1,58 @@
+<template>
+    <div>TODO</div>
+</template>
+<script>
+import Vue from 'vue'
+
+export Vue.extend({
+  props: {
+      value: {
+        type: Boolean,
+        required: true
+      },
+  },
+
+  data() {
+    return {
+      show: false,
+    }
+  },
+
+  watch: {
+    value: {
+      handler(value) {
+        this.show = value
+      },
+      immediate: true
+    },
+  },
+
+  methods: {
+    onCreateQualityIssue() {
+      this.$emit('create', {
+        isSafety: false
+      })
+      this.hide()
+    },
+
+    onCreateSafetyIssue() {
+      this.$emit('create', {
+        isSafety: true
+      })
+      this.hide()
+    },
+    onCancel() {
+      this.hide()
+      this.$emit('cancel')
+    },
+
+    hide() {
+      this.$emit('input', false)
+      this.show = false
+    }
+  },
+
+})
+
+</script>
+<style lang="less" scoped> </style>
